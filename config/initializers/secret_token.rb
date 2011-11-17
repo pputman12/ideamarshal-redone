c6060a715829a90c2c871792fc97500b2a4b550a
Patrick Putman <putman.patrick@gmail.com>
2011-11-16 20:21:37 -0600

    New site, basic layout, feedbacks form added too

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ideam::Application.config.secret_token = '38da44fa431e328500898eb403aca442ba25277b7428eb24a63fce5770f27dd343e635b4292e8ebf1fdc921b76dcf0217a7dc1b9045edd23baea6f5a8f27f2e6'
